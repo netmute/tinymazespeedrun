8dafda851e8e8e791944424c2a7387e5d3c68b80
Simon Ernst <se@netmute.org>
2012-04-30 14:37:31 +0200

    Make blocks destroyable (God mode).

--- content/js/include/game.js
+++ content/js/include/game.js
@@ -1,24 +1,27 @@
 Crafty.scene("game", function () {
 
   var fieldSize = 10,
-      fieldCount = difficulty;
-      borderThickness = 10,
-      timerSize = 20,
-      playingFieldX = (fieldSize+borderThickness)*fieldCount+borderThickness,
-      playingFieldY = (fieldSize+borderThickness)*fieldCount+borderThickness;
+    fieldCount = difficulty;
+    borderThickness = 10,
+    timerSize = 20,
+    playingFieldX = (fieldSize+borderThickness)*fieldCount+borderThickness,
+    playingFieldY = (fieldSize+borderThickness)*fieldCount+borderThickness;
 
   Crafty.init(playingFieldX, playingFieldY+timerSize);
   Crafty.background('rgb(255,255,255)');
   drawText('TinyMazeSpeedrun',timerSize - 2, 0, 0);
 
   var maze = generateMaze(fieldCount, fieldCount),
-      horizontalCenter = (fieldSize+borderThickness)*(fieldCount/2),
-      playerSize = Math.round(fieldSize*0.8),
-      paddingToBorder = Math.floor((fieldSize-playerSize)/2)+borderThickness;
+    horizontalCenter = (fieldSize+borderThickness)*(fieldCount/2),
+    playerSize = Math.round(fieldSize*0.8),
+    playerSpeed = 3,
+    paddingToBorder = Math.floor((fieldSize-playerSize)/2)+borderThickness;
 
   drawMaze(fieldCount, fieldSize, timerSize, borderThickness, maze);
   timer = drawTimer(timerSize - 2, playingFieldX);
-  createPlayer(paddingToBorder, horizontalCenter+paddingToBorder+timerSize, playerSize, 3, timer);
+  var player = createPlayer(paddingToBorder, horizontalCenter+paddingToBorder+timerSize, playerSize, playerSpeed, timer, playingFieldX, timerSize);
   createExit(playingFieldX-playerSize-paddingToBorder, horizontalCenter+paddingToBorder+timerSize, playerSize);
+
+  player.isGod = true;
 
 });
--- content/js/include/player.js
+++ content/js/include/player.js
@@ -1,34 +1,66 @@
-function createPlayer(start_x, start_y, size, speed, timer){
-  return Crafty.e("Player, 2D, Canvas, Fourway, Collision, SpriteAnimation, player")
-          .attr({ x:start_x, y:start_y, z:1 })
-          .fourway(speed)
-          .animate("walk_left", 6, 0, 8)
-          .animate("walk_right", 9, 0, 11)
-          .animate("walk_up", 3, 0, 5)
-          .animate("walk_down", 0, 0, 2)
-          .bind("NewDirection", function(direction){
-            if (direction.x < 0)
-              if (!this.isPlaying("walk_left"))
-                this.stop().animate("walk_left", 3, -1);
-            if (direction.x > 0)
-              if (!this.isPlaying("walk_right"))
-                this.stop().animate("walk_right", 3, -1);
-            if (direction.y < 0)
-              if (!this.isPlaying("walk_up"))
-                this.stop().animate("walk_up", 3, -1);
-            if (direction.y > 0)
-              if (!this.isPlaying("walk_down"))
-                this.stop().animate("walk_down", 3, -1);
-            if(!direction.x && !direction.y)
-              this.stop();
-          })
-          .bind('Moved', function(from){
-            if(this.hit('Border'))
-              this.attr({x: from.x, y:from.y});
-            if(this.hit('Exit')) {
-              timer.stop();
-              score = timer.duration();
-              Crafty.scene('outro');
-            }
-          });
+function createPlayer(start_x, start_y, size, speed, timer, maxOuterBounds, minTopBounds){
+
+  function destroyBlock(block, player, from){
+    SoundJS.play('destroy');
+    Crafty.e("2D, Canvas, debris").attr({ x:block.x, y:block.y });
+    block.destroy();
+    if (player.x != from.x)
+      player.attr({x: from.x-speed});
+    if (player.y != from.y)
+      player.attr({y: from.y-speed});
+  }
+
+  function finishGame(){
+    timer.stop();
+    score = timer.duration();
+    Crafty.scene('outro');
+  }
+
+  var player = Crafty.e("Player, 2D, Canvas, Fourway, Collision, SpriteAnimation, player")
+    .attr({ x:start_x, y:start_y, z:1 })
+    .fourway(speed)
+    .animate("walk_left", 6, 0, 8)
+    .animate("walk_right", 9, 0, 11)
+    .animate("walk_up", 3, 0, 5)
+    .animate("walk_down", 0, 0, 2)
+    .bind("NewDirection", function(direction){
+    if (direction.x < 0)
+      if (!this.isPlaying("walk_left"))
+        this.stop().animate("walk_left", 3, -1);
+    if (direction.x > 0)
+      if (!this.isPlaying("walk_right"))
+        this.stop().animate("walk_right", 3, -1);
+    if (direction.y < 0)
+      if (!this.isPlaying("walk_up"))
+        this.stop().animate("walk_up", 3, -1);
+    if (direction.y > 0)
+      if (!this.isPlaying("walk_down"))
+        this.stop().animate("walk_down", 3, -1);
+    if(!direction.x && !direction.y)
+      this.stop();
+    })
+  .bind('Moved', function(from){
+    // detect collision with blocks
+    if (this.hit('Border')) {
+      if (this.isGod) {
+        var block = _.first(this.hit('Border')).obj
+        destroyBlock(block, this, from);
+      } else {
+        this.attr({x: from.x, y: from.y});
+      }
+    }
+    // detect if exit reached
+    if (this.hit('Exit')) {
+      finishGame();
+    }
+    // detect if player is leaving the playing field
+    if ((this.x < 0) ||
+      (this.x > maxOuterBounds) ||
+      (this.y < minTopBounds) ||
+      (this.y > maxOuterBounds + minTopBounds)) {
+        SoundJS.play('teleport');
+        this.attr({x: start_x, y:start_y});
+    }
+  });
+  return player;
 }
